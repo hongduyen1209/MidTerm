dddb26b393c0a28cd7c4bfe6ec11febe4a16c3dc
hongduyen1209 <130289045+hongduyen1209@users.noreply.github.com>
2024-05-08 15:18:52 +0700

    Delete src/main/java/com/example/midterm/repository directory

--- src/main/java/com/example/midterm/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.midterm.repository;
-
-import com.example.midterm.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-}
